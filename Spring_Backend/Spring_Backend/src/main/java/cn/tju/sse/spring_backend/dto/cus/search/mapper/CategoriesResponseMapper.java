929e042df8fb2e9425aa0a970f0a309c9cd17be6
TJZjc <1344179523@qq.com>
2024-05-05 07:36:03 +0800

    zjc commit

---
+++ Spring_Backend/Spring_Backend/src/main/java/cn/tju/sse/spring_backend/dto/cus/search/mapper/CategoriesResponseMapper.java
@@ -0,0 +1,35 @@
+package cn.tju.sse.spring_backend.dto.cus.search.mapper;
+
+import cn.tju.sse.spring_backend.dto.cus.search.SearchCategoriesResponseDTO;
+import cn.tju.sse.spring_backend.model.CommoditiesCategoriesEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName CategoriesResponseMapper
+ * @Description 将查询到的类别实体转化为发送给前端的消息
+ */
+@Mapper
+public interface CategoriesResponseMapper {
+    CategoriesResponseMapper INSTANCE = Mappers.getMapper(CategoriesResponseMapper.class);
+
+    /**
+     * @param entities 查询到的商品类别实体
+     * @return 发送给前端的实体数组
+     * @see SearchCategoriesResponseDTO
+     * @see CommoditiesCategoriesEntity
+     */
+    default SearchCategoriesResponseDTO entitiesToResponse(CommoditiesCategoriesEntity[] entities){
+        if(entities == null){
+            return new SearchCategoriesResponseDTO();
+        }
+        String[] target = Arrays.stream(entities).
+                map(CommoditiesCategoriesEntity::getComCategory).toArray(String[]::new);
+
+        SearchCategoriesResponseDTO responseDTO = new SearchCategoriesResponseDTO();
+        responseDTO.setCom_categories(target);
+        return responseDTO;
+    }
+}
