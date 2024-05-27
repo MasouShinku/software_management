8cc2463d19b98d5cc07a7a2017da64ea2a092a7c
Wqa716 <96969116+QA716@users.noreply.github.com>
2024-05-28 00:18:03 +0800

    modify controller

---
+++ Spring_Backend/Spring_Backend/src/main/java/cn/tju/sse/spring_backend/controller/cus/appeal/AppealController.java
@@ -0,0 +1,32 @@
+package cn.tju.sse.spring_backend.controller.cus.appeal;
+
+
+import cn.tju.sse.spring_backend.dto.cus.appeal.CreateAppealRequestDTO;
+import cn.tju.sse.spring_backend.dto.cus.appeal.MsgResponseDTO;
+import cn.tju.sse.spring_backend.dto.cus.indent.RefundRequestDTO;
+import cn.tju.sse.spring_backend.dto.cus.indent.RefundResponseDTO;
+import cn.tju.sse.spring_backend.service.cus.appeal.AppealService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+
+
+@RestController
+@RequestMapping("/api/cus/appeal")
+public class AppealController {
+    @Autowired
+    AppealService appealService;
+
+    @RequestMapping(value = "/createAppeal", method = RequestMethod.POST)
+    public ResponseEntity<MsgResponseDTO> createAppeal (@RequestBody CreateAppealRequestDTO request) throws ParseException {
+
+        MsgResponseDTO responseDTO=appealService.createAppeal(request);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+}
